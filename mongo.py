a6da1d9c328302f2090fcab732c81a099e30a2b6
Snackya <13784757+Snackya@users.noreply.github.com>
2020-09-14 15:52:57 +0200

    All language support; generic db-getter
    - database script now works for all languages available from eur-lex
    - replaced specific get-functions in mongo.py with a generic one accepting all keys

--- mongo.py
+++ mongo.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient, errors
+import functools
+import timeit
 
 client = MongoClient('localhost', 27017)
 db = client.judgment_corpus
@@ -6,88 +8,54 @@ collection = client.judgment_corpus.judgments_en
 
 PRINT_DUPLICATE_ERRORS = 0
 
+AVAILABLE_LANGUAGES = ['bg', 'hr', 'cs', 'da', 'nl', 'en', 'et', 'fi', 'fr', 'de', 'el', 'hu',
+                        'ga', 'it', 'lv', 'lt', 'mt', 'pl', 'pt', 'ro', 'sk', 'sl', 'es', 'sv']
 
-def init_db(english_corpus_used = True, german_corpus_used = False):
+def change_cur_coll(language):
+    if language in AVAILABLE_LANGUAGES:
+        _collection = db["judgments_{}".format(language)]
+        return _collection
+
+def init_db(used_languages = ['en']):
     collist = db.list_collection_names()
-    if english_corpus_used:
-        if "judgments_en" in collist:
-            print("Collection already created for: English corpus")
+
+    for lang in used_languages:
+        if lang not in AVAILABLE_LANGUAGES:
+            continue
+
+        coll_name = "judgments_" + lang
+        if coll_name in collist:
+            print("DB: collection already created for:'",lang,"'")
         else:
-            db.create_collection("judgments_en", )
-            collection.create_index([('reference', -1)], unique=True)
-    if german_corpus_used:
-        if "judgments_de" in collist:
-            print("Collection already created for: German corpus")
-        else:
-            db.create_collection("judgments_de", )
+            db.create_collection(coll_name)
             collection.create_index([('reference', -1)], unique=True)
 
 
-def insert_doc(doc, corpus):
-    if corpus == 'en':
-        collection = client.judgment_corpus.judgments_en
-    elif corpus == 'de':
-        collection = client.judgment_corpus.judgments_de
-
+def insert_doc(doc, language):
+    collection = change_cur_coll(language)
     try:
         collection.insert_one(doc)
     except errors.DuplicateKeyError as e:
         if PRINT_DUPLICATE_ERRORS:
             print("document exists already")
             print(e)
+    else:
+        print("DB: insert_doc(): unsuported language")
 
+def get_all_docs(language):
+    collection = change_cur_coll(language)
+    cursor = collection.find({})
+    return cursor
 
-def get_docs_between_dates(start, end):
+def get_docs_between_dates(start, end, language):
     # retrieves documents between two dates (dates must be in the format: y-m-dT00:00:00.000+00:00)
     cursor = collection.find({'date': {'$lt': end, '$gte': start}})
     return cursor
 
-
-def get_docs_by_title(title):
-    # retrieves documents by the title
-    cursor = collection.find({'title': title})
-    return cursor
-
-
-def get_docs_by_reference(reference):
-    # retrieves documents by the reference number
-    cursor = collection.find({'reference': reference})
-    return cursor
-
-
-def get_docs_by_id(_id):
-    # retrieves documents by the mongoDb _id
-    cursor = collection.find({'_id': _id})
-    return cursor
-
-
-def get_docs_by_celex(celex):
-    # retrieves documents by the celex number
-    cursor = collection.find({'celex': celex})
-    return cursor
-
-
-def get_docs_by_date(date):
-    # retrieves documents by the date (date must be in the format: y-m-dT00:00:00.000+00:00)
-    cursor = collection.find({'date': date})
-    return cursor
-
-
-def get_docs_by_ecli(ecli):
-    # retrieves documents by the ecli number
-    cursor = collection.find({'ecli': ecli})
-    return cursor
-
-
-def get_docs_by_case_affecting(case_affecting):
-    # retrieves documents by the case_affecting value
-    cursor = collection.find({'case_affecting': case_affecting})
-    return cursor
-
-
-def get_docs_by_object_value(column, value):
+def get_docs_by_object_value(column, value, language):
     # retrieves documents by searching a object column for a specified value
     # this DB request requires JavaScript
+    collection = change_cur_coll(language)
     cursor = collection.find({"$where":
     '''
         function() {
@@ -103,14 +71,16 @@ def get_docs_by_object_value(column, value):
     return cursor
 
 
-def get_docs_by_object_key(column, key):
+def get_docs_by_object_key(column, key, language):
     # retrieves documents by searching a object column for a specified value
+    collection = change_cur_coll(language)
     cursor = collection.find({column + "." + key: {"$exists": True}})
     return cursor
 
 
-def get_docs_search_string(column, search):
+def get_docs_search_string(column, search, language):
     # retrieves documents by searching a string column with specified words (words separated by whitespace)
+    collection = change_cur_coll(language)
     search_words = search.split(" ")
     search_string = ""
     for word in search_words:
@@ -120,8 +90,12 @@ def get_docs_search_string(column, search):
     cursor = collection.find({column: {"$regex": search_string, "$options": "i"}})
     return cursor
 
+def get_docs_by_value(column, value, language):
+    collection = change_cur_coll(language)
+    cursor = collection.find({column: value})
+    return cursor
 
-init_db(english_corpus_used = True, german_corpus_used = True)
+init_db(used_languages=['en', 'de'])
 
-# for doc in get_docs_by_object_value("author", "Court of Justice"):
-#     print(doc.get('author'))
+for doc in get_all_docs('en'):
+    print(doc.get('celex'))
