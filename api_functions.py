dd2d64809792f5cbf44b68800a081d2079e7774f
Philipp Hartl <Philipp1.Hartl@stud.uni-regensburg.de>
2020-10-21 19:39:52 +0200

    Api calls now respect default values correctly (done by **kwargs mapping). Some api keywords have been changed to match the function keywords of analysis.py. Fixed some minor bugs in analysis & api. Tested all api functions.

--- api_functions.py
+++ api_functions.py
@@ -9,6 +9,7 @@ analysers = {
     "de": None
 }
 
+
 def add_analyser(language):
     global analysers
     if language == "en" and not analysers.get("en"):
@@ -17,7 +18,7 @@ def add_analyser(language):
     elif language == "de" and not analysers.get("de"):
         print("Loading german models")
         analysers["de"] = CorpusAnalysis("de")
-    
+
 
 def __get_top_occurrences(_list, limit=None):
     # create list with parsed strings because custom types like Token cannot be string compared
@@ -33,11 +34,13 @@ def __get_top_occurrences(_list, limit=None):
         occurrence_list = Counter(string_list).most_common()
     return occurrence_list
 
+
 def __to_sentence_list(span_list):
     sentences = []
     for span in span_list:
         sentences.append(str(span))
     return sentences
+
 
 def __to_tuple_list(token_tuples):
     tuples = []
@@ -45,30 +48,40 @@ def __to_tuple_list(token_tuples):
         tuples.append((str(item[0]), str(item[1])))
     return tuples
 
+def __to_tuple_list_int(token_tuples):
+    tuples = []
+    for item in token_tuples:
+        tuples.append((str(item[0]), int(item[1])))
+    return tuples
+
+
+def __get_local_kwargs(args, possible_args):
+    local_args = {}
+    for arg in possible_args:
+        if args.get(arg) != None:
+            local_args[arg] = args.get(arg)
+    return local_args
+
+
 def __run_analysis(analyser, args):
-    #--------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     # please refer to analysis.py for documentation on every analysis operation
-    #--------------------------------------------------------------------------
+    # --------------------------------------------------------------------------
     analysis_data = None
     arg_type = args.get('type')
 
     # n-grams
     if arg_type == 'n-grams':
         analysis_data = __get_top_occurrences(
-            analyser.get_n_grams(args.get('n'),
-                                args.get('remove_stopwords'),
-                                args.get('filter_nums'),
-                                args.get('min_freq')),
+            analyser.get_n_grams(**__get_local_kwargs(args, ("n", "remove_stopwords", "filter_nums", "min_freq"))),
             args.get('limit')
         )
     elif arg_type == 'n-grams_per_doc':
         results = []
-        ngram_list = analyser.get_n_grams_per_doc(args.get('n'),
-                                                args.get('remove_stopwords'),
-                                                args.get('filter_nums'),
-                                                args.get('min_freq'))
+        ngram_list = analyser.get_n_grams_per_doc(
+            **__get_local_kwargs(args, ("n", "remove_stopwords", "filter_nums", "min_freq_per_doc")))
         for entry in ngram_list:
-            results.append(__get_top_occurrences(entry, args.get('limit'))) 
+            results.append(__get_top_occurrences(entry, args.get('limit')))
         analysis_data = results
 
     # readability
@@ -77,79 +90,67 @@ def __run_analysis(analyser, args):
     elif arg_type == 'readability_per_doc':
         analysis_data = analyser.get_readability_score_per_doc()
     elif arg_type == 'average_readability':
-        analysis_data = analyser.get_average_readability_score() 
+        analysis_data = analyser.get_average_readability_score()
 
     # tokens
     elif arg_type == 'tokens':
         analysis_data = __get_top_occurrences(
-            analyser.get_tokens(args.get('remove_punctuation'),
-                                args.get('remove_stopwords'),
-                                args.get('include_pos'),
-                                args.get('exclude_pos'),
-                                args.get('min_freq')),
+            analyser.get_tokens(**__get_local_kwargs(args, ("remove_punctuation", "remove_stopwords",
+                                                            "include_pos", "exclude_pos", "min_freq_per_doc"))),
             args.get('limit')
         )
     elif arg_type == 'tokens_per_doc':
         results = []
-        docs = analyser.get_tokens_per_doc(args.get('remove_punctuation'),
-                                            args.get('remove_stopwords'),
-                                            args.get('include_pos'),
-                                            args.get('exclude_pos'),
-                                            args.get('min_freq'))
+        docs = analyser.get_tokens_per_doc(
+            **__get_local_kwargs(args, ("remove_punctuation", "remove_stopwords", "include_pos", "exclude_pos", "min_freq_per_doc")))
         for doc_result in docs:
-            results.append(__get_top_occurrences(doc_result), args.get('limit'))
+            results.append(__get_top_occurrences(doc_result, args.get('limit')))
         analysis_data = results
+
     elif arg_type == 'unique_tokens':
-        analysis_data = analyser.get_unique_tokens(args.get('remove_punctuation'),
-                                            args.get('remove_stopwords'),
-                                            args.get('include_pos'),
-                                            args.get('exclude_pos'),
-                                            args.get('min_freq'))
+        analysis_data = analyser.get_unique_tokens(
+            **__get_local_kwargs(args, ("remove_punctuation", "remove_stopwords", "include_pos", "exclude_pos", "min_freq_per_doc")))
+
     elif arg_type == 'unique_tokens_per_doc':
-        analysis_data = analyser.get_unique_tokens_per_doc(args.get('remove_punctuation'),
-                                            args.get('remove_stopwords'),
-                                            args.get('include_pos'),
-                                            args.get('exclude_pos'),
-                                            args.get('min_freq'))
+        analysis_data = analyser.get_unique_tokens_per_doc(
+            **__get_local_kwargs(args, ("remove_punctuation", "remove_stopwords", "include_pos", "exclude_pos", "min_freq_per_doc")))
+
     elif arg_type == 'average_token_length':
-        analysis_data = analyser.get_average_token_length(args.get('remove_punctuation'),
-                                                        args.get('remove_stopwords'),
-                                                        args.get('include_pos'),
-                                                        args.get('exclude_pos'),
-                                                        args.get('min_freq'))
+        analysis_data = analyser.get_average_token_length(
+            **__get_local_kwargs(args, ("remove_punctuation", "remove_stopwords", "include_pos", "exclude_pos", "min_freq")))
+
+    elif arg_type == 'average_word_length':
+        analysis_data = analyser.get_average_token_length(
+            True, **__get_local_kwargs(args, ("remove_stopwords", "include_pos", "exclude_pos", "min_freq")))
+
     elif arg_type == 'token_count':
-        analysis_data = analyser.get_token_count(args.get('remove_punctuation'),
-                                                args.get('remove_stopwords'),
-                                                args.get('include_pos'),
-                                                args.get('exclude_pos'),
-                                                args.get('min_freq'))
+        analysis_data = analyser.get_token_count(
+            **__get_local_kwargs(args, ("remove_punctuation", "remove_stopwords", "include_pos", "exclude_pos", "min_freq_per_doc")))
+
     elif arg_type == 'token_count_per_doc':
-        analysis_data = analyser.get_token_count_per_doc(args.get('remove_punctuation'),
-                                                        args.get('remove_stopwords'),
-                                                        args.get('include_pos'),
-                                                        args.get('exclude_pos'),
-                                                        args.get('min_freq'))
+        analysis_data = analyser.get_token_count_per_doc(
+            **__get_local_kwargs(args, ("remove_punctuation", "remove_stopwords", "include_pos", "exclude_pos", "min_freq_per_doc")))
+
     elif arg_type == 'most_frequent_words':
-        analysis_data = analyser.get_most_frequent_words(args.get('remove_stopwords'),
-                                                        args.get('lemmatise'),
-                                                        args.get('limit'))
+        analysis_data = analyser.get_most_frequent_words(
+            **__get_local_kwargs(args, ("remove_stopwords", "lemmatize", "n")))
+
     elif arg_type == 'most_frequent_words_per_doc':
-        analysis_data = analyser.get_most_frequent_words(args.get('remove_stopwords'),
-                                                        args.get('lemmatise'),
-                                                        args.get('limit'))
+        results = []
+        docs = analyser.get_most_frequent_words_per_doc(
+            **__get_local_kwargs(args, ("remove_stopwords", "lemmatize", "n")))
+        for doc_result in docs:
+            results.append(__to_tuple_list_int(doc_result))
+        analysis_data = results
 
     # lemmata
     elif arg_type == 'lemmata':
         analysis_data = __to_tuple_list(
-            analyser.get_lemmata(args.get('remove_stopwords'),
-                                args.get('include_pos'),
-                                args.get('exclude_pos'))
-        )
+            analyser.get_lemmata(**__get_local_kwargs(args, ("remove_stopwords", "include_pos", "exclude_pos"))))
     elif arg_type == 'lemmata_per_doc':
         results = []
-        docs = analyser.get_pos_tags_per_doc(args.get('remove_stopwords'),
-                                            args.get('incl_pos'),
-                                            args.get('excl_pos'))
+        docs = analyser.get_lemmata_tags_per_doc(
+            **__get_local_kwargs(args, ("remove_stopwords", "include_pos", "exclude_pos")))
         for doc_result in docs:
             results.append(__to_tuple_list(doc_result))
         analysis_data = results
@@ -157,13 +158,11 @@ def __run_analysis(analyser, args):
     # pos tags
     elif arg_type == 'pos_tags':
         analysis_data = __to_tuple_list(
-            analyser.get_pos_tags(args.get('incl_pos'),
-                                args.get('excl_pos'))
-        )
+            analyser.get_pos_tags(**__get_local_kwargs(args, ("include_pos", "exclude_pos"))))
     elif arg_type == 'pos_tags_per_doc':
         results = []
-        docs = analyser.get_pos_tags_per_doc(args.get('incl_pos'),
-                                            args.get('excl_pos'))
+        docs = analyser.get_pos_tags_per_doc(
+            **__get_local_kwargs(args, ("remove_stopwords", "include_pos", "exclude_pos")))
         for doc_result in docs:
             results.append(__to_tuple_list(doc_result))
         analysis_data = results
@@ -195,9 +194,9 @@ def __run_analysis(analyser, args):
 
     # keywords
     elif arg_type == 'keywords':
-        analysis_data = analyser.get_keywords(args.get('limit'))
+        analysis_data = analyser.get_keywords(**__get_local_kwargs(args, ("top_n")))
     elif arg_type == 'keywords_per_doc':
-        analysis_data = analyser.get_keywords_per_doc(args.get('limit'))
+        analysis_data = analyser.get_keywords_per_doc(**__get_local_kwargs(args, ("top_n")))
 
     # misc
     elif arg_type == 'similarity':
@@ -207,11 +206,12 @@ def __run_analysis(analyser, args):
 
     return analysis_data
 
+
 def analyse_corpus(corpus, args_list, language):
     global analysers
     if not analysers.get(language):
         add_analyser(language)
-    
+
     texts = []
     analysis_data = {}
 
@@ -223,8 +223,8 @@ def analyse_corpus(corpus, args_list, language):
     analysis_types = []
     for args in args_list:
         analysis_types.append(args.get('type'))
-    
-    analysers[language].exec_pipeline(texts, pipeline_components = analysis_types)
+
+    analysers[language].exec_pipeline(texts, pipeline_components=analysis_types)
 
     for args in args_list:
         arg_type = args.get('type')
@@ -233,31 +233,32 @@ def analyse_corpus(corpus, args_list, language):
     # del analyser
     return analysis_data
 
-#-------------------
+
+# -------------------
 # test custom query
-#-------------------
+# -------------------
 test_query = [
-        # {
-        #     "operator": "NOT",
-        #     "search identifier": True,
-        #     "column": "case_law_directory",
-        #     "value": 'F'
-        # },
-        # {
-        #     "operator" : "NOT",
-        #     "search identifier": True,
-        #     "column": 'applicant',
-        #     "value": 'Person'
-        # },
-        {
-            "column": 'celex',
-            "value": ['61956CJ0007', '61958CJ0022', '61959CJ0025']
-        },
-        {
-            "column": "date",
-            "start date": "1958-07-17",
-            "end date": "1959-07-17" 
-        }
+    # {
+    #     "operator": "NOT",
+    #     "search identifier": True,
+    #     "column": "case_law_directory",
+    #     "value": 'F'
+    # },
+    # {
+    #     "operator" : "NOT",
+    #     "search identifier": True,
+    #     "column": 'applicant',
+    #     "value": 'Person'
+    # },
+    {
+        "column": 'celex',
+        "value": ['61956CJ0007', '61958CJ0022', '61959CJ0025']
+    },
+    {
+        "column": "date",
+        "start date": "1958-07-17",
+        "end date": "1959-07-17"
+    }
 ]
 # 61956CJ0007 has cld = C and F
 # 61958CJ0022 has cld not Fm applicant PART
